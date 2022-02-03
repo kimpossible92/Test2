9029ea5a0f94fe9bda80853ef812b11fb8b5588a
kimpossible92 <kim.possible.92@bk.ru>
2022-02-03 09:15:25 +0900

    first commit

---
+++ Assets/Arrays.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+    [SerializeField] OpenAppLevel levelsManager2;
+    public static Arrays THIS;
+    public int SizeX = 9;
+    public int SizeY = 11;
+    public Gem[,] gems;
+    Gem GetGem1;
+    Gem GetGem2;
+    public Gem this[int r, int c]
+    {
+        get { return gems[r, c]; }
+        set { gems[r, c] = value; }
+    }
+    public HitCandy[] ingredientsGems;
+    public HitCandy[] ingredientsGems2;
+    public HitCandy IngredientCurrent;
+    public Gem currentGem;
+    // Use this for initialization
+    void Start() {
+    }
+    private void Awake()
+    {
+        THIS = this;
+        gems = new Gem[SizeY, SizeX];
+        for (int row = 0; row < SizeY; row++)
+        {
+            for (int c = 0; c < SizeX; c++)
+            {
+                gems[row, c] = new Gem(row, c);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public NeighbourProp GetNewProp(Gem gem1,Gem gem2)
+    {
+        NeighbourProp neighbour = new NeighbourProp();
+        IEnumerable<Gem> nhMatch = matchesOnes(gem1,gem2);
+        foreach(var g in nhMatch)
+        {
+            neighbour.gemms.Add(g);
+        }
+        return neighbour;
+    }
+    private IEnumerable<Gem> matchesOnes(Gem gem1,Gem gem2)
+    {
+        List<Gem> hormatch = new List<Gem>();
+        hormatch.Add(gem1);
+        if (gem1.match3(gem2))
+        {
+            hormatch.Add(gem2);
+        }
+        if (hormatch.Count() < 2)
+        {
+            hormatch.Clear();
+        }
+        return hormatch.Distinct();
+    }
+    public bool IsNulls(int row,int col)
+    {
+        if (levelsManager2.blocksp[row*levelsManager2.MaxX+col].types==0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public IEnumerable<Gem> NullGemsonc(int collum)
+    {
+        List<Gem> gemsnull = new List<Gem>();
+        for (int i = 0; i < SizeY; i++)
+        {
+            if (gems[i, collum].INull||IsNulls(i,collum)==true||levelsManager2.blocksp[i*levelsManager2.MaxX+collum].types==0)
+            {
+                gemsnull.Add(gems[i, collum]);
+            }
+        }
+        return gemsnull;
+    }
+}
---
+++ Assets/MenuGUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGUI : MonoBehaviour
+{
+    public void MenuTounamentClick()
+    {
+        if (PlayerPrefs.GetInt("TourQual") >= 10)
+        {
+        }
+        else
+        {
+            GameObject.Find("CanvasGlobal").transform.Find("TournamentQualification").gameObject.SetActive(true);
+            if (PlayerPrefs.GetInt("TourQual") >= 10)
+            {
+                GameObject.Find("CanvasGlobal").transform.Find("TournamentQualification").transform.Find("Image").transform.Find("Join").gameObject.SetActive(true);
+                GameObject.Find("CanvasGlobal").transform.Find("TournamentQualification").transform.Find("Image").transform.Find("CheckTarget").transform.Find("Check").gameObject.SetActive(true);
+            }
+            else
+            {
+                GameObject.Find("CanvasGlobal").transform.Find("TournamentQualification").transform.Find("Image").transform.Find("Join").gameObject.SetActive(false);
+                GameObject.Find("CanvasGlobal").transform.Find("TournamentQualification").transform.Find("Image").transform.Find("CheckTarget").transform.Find("Check").gameObject.SetActive(false);
+            }
+            //Tournament.tournament.kubokButton();
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PlayerPrefs.SetInt("TourQual", (PlayerPrefs.GetInt("TourQual")+10));
+        }
+    }
+}
